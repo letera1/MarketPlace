b94d9a25a275f8b5a8717a0c9f8730ca1f19222f
Letera Tujo <168364436+letera1@users.noreply.github.com>
2025-07-09 12:55:04 +0300

    Update Choose.jsx

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -11,7 +11,7 @@ export default function Choose() {
       <div className="content">
         <h2>Why Should you Choose Our Website ?</h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
+          Lorem Ipsum is simply dummy ofroso text of the printing and typesetting
           industry. Lorem hulo slide Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book.
